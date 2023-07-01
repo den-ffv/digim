35ed6883f6da27b5ed072942bd8a68c0207894b5
Bohdan C <105508224+den-ffv@users.noreply.github.com>
2023-07-01 15:39:45 +0300

    add block team

---
+++ src/components/Team/Team.jsx
@@ -0,0 +1,23 @@
+import "./Team.scss";
+import linkedin from "../../assets/linkedinIcon2.svg";
+import vector from "../../assets/vector.svg";
+function Team({ img, userName, title, text, link }) {
+  return (
+    <div className='team'>
+      <div className='team__content'>
+        {/* <div className="team__img-vector"> */}
+          <img className='team__img' src={vector} alt='' />
+          {/* <img className='team__img' src={img} alt='' /> */}
+        {/* </div> */}
+        <h4 className='team__user-name'>{userName}</h4>
+        <h4 className='team__title'>{title}</h4>
+        <p className='team__text'>{text}</p>
+      </div>
+      <a className='team__link' href={`https://www.linkedin.com/in/${link}`}>
+        <img src={linkedin} alt='' />
+      </a>
+    </div>
+  );
+}
+
+export default Team;
